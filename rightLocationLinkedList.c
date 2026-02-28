0b8e145319d3e3b4d14b76d4a0426c0309139fd8
Raaajitis <rajvardhansinghyadav2006@gmail.com>
2026-02-28 23:56:16 +0530

    Add files via upload

---
+++ rightLocationLinkedList.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Create new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Rotate list
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int length = 1;
+
+    // Find length
+    while (temp->next != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    k = k % length;
+    if (k == 0)
+        return head;
+
+    // Make circular
+    temp->next = head;
+
+    int steps = length - k;
+    struct Node* newTail = head;
+
+    for (int i = 1; i < steps; i++)
+        newTail = newTail->next;
+
+    struct Node* newHead = newTail->next;
+    newTail->next = NULL;
+
+    return newHead;
+}
+
+// Print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
+
+    int k = 2;
+
+    head = rotateRight(head, k);
+    printList(head);
+
+    return 0;
+}
