56ceaad91b3f6ab1ea35e642c78158e901ef8c5f
Raaajitis <rajvardhansinghyadav2006@gmail.com>
2026-03-02 23:06:46 +0530

    Add files via upload

---
+++ KthSmallestElement.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+// Swap function
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Partition function
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+
+    swap(&arr[i], &arr[high]);
+    return i;
+}
+
+// QuickSelect function
+int quickSelect(int arr[], int low, int high, int k) {
+    if (low <= high) {
+        int pi = partition(arr, low, high);
+
+        if (pi == k - 1)
+            return arr[pi];
+        else if (pi > k - 1)
+            return quickSelect(arr, low, pi - 1, k);
+        else
+            return quickSelect(arr, pi + 1, high, k);
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 3;
+
+    printf("Kth Smallest Element: %d\n",
+           quickSelect(arr, 0, n - 1, k));
+
+    return 0;
+}
