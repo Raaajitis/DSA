fe468d1a56768c47416aa74114fb87afd23d6025
Raaajitis <rajvardhansinghyadav2006@gmail.com>
2026-03-03 23:05:15 +0530

    Add files via upload

---
+++ ImplementMinStack.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int stack[MAX], minStack[MAX];
+int top = -1, minTop = -1;
+
+void push(int x) {
+    stack[++top] = x;
+
+    if (minTop == -1 || x <= minStack[minTop])
+        minStack[++minTop] = x;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+
+    if (stack[top] == minStack[minTop])
+        minTop--;
+
+    top--;
+}
+
+int getTop() {
+    if (top == -1)
+        return -1;
+    return stack[top];
+}
+
+int getMin() {
+    if (minTop == -1)
+        return -1;
+    return minStack[minTop];
+}
+
+int main() {
+    push(5);
+    push(3);
+    push(7);
+
+    printf("Min: %d\n", getMin());  // 3
+
+    pop();
+    printf("Min: %d\n", getMin());  // 3
+
+    pop();
+    printf("Min: %d\n", getMin());  // 5
+
+    return 0;
+}
