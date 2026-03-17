ee28de034ee283c7f3a4103191a2b64c9c61c83e
Raaajitis <rajvardhansinghyadav2006@gmail.com>
2026-03-17 23:45:22 +0530

    Add files via upload

---
+++ SlidingWindowMaximum.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX 100
+
+void slidingWindowMax(int arr[], int n, int k) {
+
+    int dq[MAX]; // stores indices
+    int front = 0, rear = -1;
+
+    for (int i = 0; i < n; i++) {
+
+        // Remove indices out of window
+        if (front <= rear && dq[front] <= i - k)
+            front++;
+
+        // Remove smaller elements
+        while (front <= rear && arr[dq[rear]] <= arr[i])
+            rear--;
+
+        dq[++rear] = i;
+
+        // Print max for window
+        if (i >= k - 1)
+            printf("%d ", arr[dq[front]]);
+    }
+}
+
+int main() {
+
+    int arr[] = {1,3,-1,-3,5,3,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 3;
+
+    slidingWindowMax(arr, n, k);
+
+    return 0;
+}
